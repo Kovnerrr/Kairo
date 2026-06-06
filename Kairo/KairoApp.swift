35d177099dc86301acad5be0aa3f8752298c30ae
Andrey Kovner <kovnerrr@gmail.com>
2026-06-06 15:07:10 +0200

    Initial Commit

---
+++ Kairo/KairoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KairoApp.swift
+//  Kairo
+//
+//  Created by Andrii Kovner on 06.06.26.
+//
+
+import SwiftUI
+
+@main
+struct KairoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KairoTests/KairoTests.swift
@@ -0,0 +1,19 @@
+//
+//  KairoTests.swift
+//  KairoTests
+//
+//  Created by Andrii Kovner on 06.06.26.
+//
+
+import Testing
+@testable import Kairo
+
+struct KairoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
